35781ea160cb309d4a5152e2812be7b572256f9b
Tomisin <fadahunsitomisin2017@gmail.com>
2024-07-30 20:02:34 +0100

    new changes

--- expenseapi/settings.py
+++ expenseapi/settings.py
@@ -84,6 +84,10 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'NON_FIELD_ERRORS_KEY': 'error'
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
